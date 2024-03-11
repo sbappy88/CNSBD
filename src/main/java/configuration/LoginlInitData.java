60760ddc29161efeed647141c94e107a66c6aae8
Abu Md. Salahuddin <111044141+sbappy88@users.noreply.github.com>
2024-03-11 09:53:10 +0600

    Update LoginlInitData.java

--- src/main/java/configuration/LoginlInitData.java
+++ src/main/java/configuration/LoginlInitData.java
@@ -76,32 +76,7 @@ public class LoginlInitData extends MainInitData
         factory_QC_manager_login = getScriptValue("factory_QC_manager_login");
         admin_login = getScriptValue("admin_login");
 
-        // Load Google Campaign related all test data into array
-//        valadd_campaign_google = getScriptValue("add_Campaign_google");
-//        valedit_campaign_google = getScriptValue("edit_Campaign_google");
-//        valadd_adgroup_google = getScriptValue("add_AdGroup_google");
-//        valedit_adgroup_google = getScriptValue("edit_AdGroup_google");
-//
-//        valadd_exactkeyword_google = getScriptValue("add_ExactKeyword_google");
-//        valadd_phrasekeyword_google = getScriptValue("add_PhraseKeyword_google");
-//        valadd_broadkeyword_google = getScriptValue("add_BroadKeyword_google");
-//
-//        valedit_exactkeyword_google = getScriptValue("edit_ExactKeyword_google");
-//        valedit_phrasekeyword_google = getScriptValue("edit_PhraseKeyword_google");
-//        valedit_broadkeyword_google = getScriptValue("edit_BroadKeyword_google");
-//
-//        valadd_textads_google = getScriptValue("add_TextAds_google");
-//        valadd_mobile_google = getScriptValue("add_Mobile_google");
-//        valadd_mobile_image_google = getScriptValue("add_Mobile_Image_google");
-//        valadd_mobcarriers_google = getScriptValue("add_Mobile_Carriers_google");
-//        valadd_mob_markuplang_google = getScriptValue("add_Mobile_Markup_Languages_google");
-//        valadd_negkeyword_google = getScriptValue("add_NegativeKeywords_google");
-//        valadd_placement_google = getScriptValue("add_Placement_google");
-
-        // Load Bing Campaign related all test data into array
-//        valadd_campaign_bing = getScriptValue("add_campaign_bing");
-//        valedit_campaign_bing = getScriptValue("edit_campaign_bing");
-
+        
     }
 }
 
